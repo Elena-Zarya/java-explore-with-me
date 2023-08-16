98efb0a884f64efdb1d4c313cfcb0315e4699574
Elena <zaryanskaya@yandex.ru>
2023-08-16 22:04:35 +0300

    Диплом. Этап 2

--- main-service/src/main/java/ru/practicum/mainservice/Pages.java
+++ main-service/src/main/java/ru/practicum/mainservice/Pages.java
@@ -1,7 +1,6 @@
 package ru.practicum.mainservice;
 
 import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
 
 public class Pages {
     public static PageRequest getPage(int from, int size) {
--- main-service/src/main/java/ru/practicum/mainservice/exception/ApiError.java
+++ main-service/src/main/java/ru/practicum/mainservice/exception/ApiError.java
@@ -3,8 +3,6 @@ package ru.practicum.mainservice.exception;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
-import java.util.List;
-
 @AllArgsConstructor
 @Getter
 public class ApiError {
